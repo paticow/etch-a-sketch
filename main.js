c7b5a8b5eea1c691c4b388af4c1320f23c781a74
Patricia Mata <patimatanazareth@gmail.com>
2025-06-30 18:44:07 -0400

    Finish the initial grid

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-//const divContainer = document.getElementById("container");
+document.addEventListener("DOMContentLoaded", function () {
 
-function createDiv(){
-            const newDiv = document.createElement("div");
-            newDiv.classList.add('div-style');
-            document.body.appendChild(newDiv);      
+function createContainer() { 
+    const newContainer = document.createElement("div");
+    newContainer.classList.add('div-container'); 
+    document.body.appendChild(newContainer); 
+
+    for (let j = 0; j < 16; j++) { 
+        const newDiv = document.createElement("div"); 
+        newDiv.classList.add('div-style'); 
+        newContainer.appendChild(newDiv); 
+    } 
 }
 
-for (let i = 0; i <= 16; i++) {
-    for (let j = 0; j <= 16; j++) {
-        //console.log(`hola ${i}`);
-        createDiv();        
-    }
+
+for (let i = 0; i < 16; i++) {
+    createContainer(); 
 }
+
+});
