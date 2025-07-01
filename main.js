d25bbf87ac6a968b8a8560c5f4df3f449f0abe74
Patricia Mata <patimatanazareth@gmail.com>
2025-07-01 17:32:38 -0400

    Finish the base project

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
+let gridDimensions = 16;
+let divContainer;
+
 document.addEventListener("DOMContentLoaded", function () {
-
-function createContainer() { 
-    const newContainer = document.createElement("div");
-    newContainer.classList.add('div-container'); 
-    document.body.appendChild(newContainer); 
-
-    for (let j = 0; j < 16; j++) { 
-        const newDiv = document.createElement("div"); 
-        newDiv.classList.add('div-style'); 
-        newContainer.appendChild(newDiv); 
-    } 
-}
-
-for (let i = 0; i < 16; i++) {
-    createContainer(); 
-}
+    divContainer = document.getElementById("container");
+    for (let i = 0; i < gridDimensions; i++) {
+        createContainer();
+    }
 });
 
+function createContainer() {
+    const newContainer = document.createElement("div");
+    newContainer.classList.add("div-container");
+    divContainer.appendChild(newContainer);
+
+    for (let j = 0; j < gridDimensions; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("div-style");
+        newContainer.appendChild(newDiv);
+    }
+}
+
+function gridSize() {
+    let size = prompt("Please enter the dimensions of your grid");
+    if (size < 101) {
+        gridDimensions = size;
+        divContainer.innerHTML = ""; 
+        for (let i = 0; i < gridDimensions; i++) {
+            createContainer();
+        }
+    } else {
+        alert("The dimensions of the grid can't excede 100");
+    }
+}
